219ccfb983dd74643b3510fbcda4dddcbd50b57a
hubi19 <hsolecki1@gmail.com>
2023-04-05 14:46:38 +0200

    2023-04-5 HS

--- scraper.py
+++ scraper.py
@@ -9,8 +9,26 @@ url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
 page_dom = BeautifulSoup(response.text, "html.parser")
 opinions = page_dom.select("div.js_product-review")
+
+def get_cos(ancestor,selector):
+    return ancestor.select_one("span.user-post__author-recomendation > em").text.strip()
+
+all_opinions = []
 for opinion in opinions:
-    print(opinion["data-entry-id"])
-print(type((page_dom)))
-print(type(opinions))
-print(len(opinions))
+    single_opinion = {
+        "opinion_id": opinion["data-entry-id"],
+        "author": opinion.select_one("span.user-post__author-name").text.strip(),
+        "recommendation": opinion.select_one("span.user-post__author-recomendation > em").text.strip(),
+        "stars": opinion.select_one("span.user-post__score-count").text.strip(),
+        "purchased": opinion.select_one("div.review-pz").text.strip(),
+        "opinion_date": opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip(),
+        "purchase_date": opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"].strip(),
+        "useful": opinion.select_one("button.vote-yes")["data-total-vote"].strip(),
+        "unuseful": opinion.select_one("button.vote-no")["data-total-vote"].strip(),
+        "content": opinion.select_one("div.user-post__text").text.strip(),
+        "cons": [cons.text.strip() for cons in opinion.select("div.review-feature__title--negatives ~ div.review-feature__item")],
+        "pros": [pros.text.strip() for pros in opinion.select("div.review-feature__title--positives ~ div.review-feature__item")],
+    }
+    all_opinions.append(single_opinion)
+
+print(all_opinions)
